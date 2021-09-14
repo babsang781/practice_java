6ad71c2c5c26f73f2cf10a48fdd39358c1094cd1
babsang781 <89438832+babsang781@users.noreply.github.com>
2021-09-14 23:54:04 +0900

    Add files via upload

---
+++ number.java
@@ -0,0 +1,12 @@
+package data_operation;
+
+public class number {
+
+	public static void main(String[] args) {
+		System.out.println("number");
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI));
+
+	}
+
+}
---
+++ string.java
@@ -0,0 +1,14 @@
+package data_operation;
+
+public class string {
+	public static void main(String[] args) {
+		System.out.println("hello \n world");
+		System.out.println("hello \"world\"");
+        int a = 1;
+        System.out.println(a);
+        
+        String c = "hello world";
+        System.out.println(c);
+	}
+
+}
