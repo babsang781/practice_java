ac92fe2f3fa8d6a0296ddcb089a216638aaa41b9
babsang781 <89438832+babsang781@users.noreply.github.com>
2021-11-19 17:40:41 +0900

    Add files via upload

--- SHRD/Ex211118_02.java
+++ SHRD/Ex211118_02.java
@@ -1,19 +1,28 @@
-package hw211119;
+package hw211118;
+
+import java.util.Scanner;
 
 public class Ex211118_02 {
-
 	public static void main(String[] args) {
-		int i = 1;// 변수 1-77
-		int j = 77;// 변수 77-1
-		int sum = 0;// 합계 변수
-		
-		for(i=1;i<=77;i++){ // 반복 77
-		
-			sum = sum + i*j;
-			j--;	
-		}
-		System.out.println(sum);
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("현재금액 : ");
+		int money = sc.nextInt();
+
+		int num1 = money / 10000;
+		int num2 = (money % 10000) / 5000;
+		int num3 = (money % 5000) / 1000;
+		int num4 = (money % 1000) / 500;
+		int num5 = (money % 500) / 100;
+
+		System.out.println("잔돈반환");
+		System.out.println("10000원: " + num1 + "개");
+		System.out.println("5000원: " + num2 + "개");
+		System.out.println("1000원: " + num3 + "개");
+		System.out.println("500원: " + num4 + "개");
+		System.out.println("100원: " + num5 + "개");
+
+		sc.close();
 	}
-
 }
