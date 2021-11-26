058a1badd02414c2e47e96a8fecf5c8e81637f73
babsang781 <89438832+babsang781@users.noreply.github.com>
2021-11-26 22:50:37 +0900

    공포의 배열 다이아
    결국 손으로 하나 하나 다 입력 하다 보니까 어떻게 할 지 생각이나더라 ㅋㅋㅋㅋ 신기하네 ㅋㅋ

---
+++ SHRD/Ex201126_03_dia.java
@@ -0,0 +1,47 @@
+package hw211126;
+
+public class Ex201126_03_dia {
+
+	public static void main(String[] args) {
+
+		int[][] arr = new int[7][7];
+		int cnt = 0;
+		int n = 7;
+
+		for (int i = 0; i < n; i++) { // 다 0 입력
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = 0;
+			}
+		}
+
+		for (int i = 0, j = 0; i < n; i++) {
+
+			if (i <= 3) { // 열 시작 번호 감소 조건
+				j = 3 - i;
+
+				// 열번호 감소시 반복횟수 지정
+				for (int k = 1; k <= i * 2 + 1; k++) { 
+					cnt++;	
+					arr[i][j] = cnt;
+					j++; // 반복하는 동안 열자리 위치 변경
+				}
+
+			} else if (i > 3) {  // 열 시작 번호 증가 조건
+				j = i - 3;
+				// 열번호 증가시 반복횟수 지정
+				for (int k = 1; k <= (6 - i) * 2 + 1; k++) {
+					cnt++;
+					arr[i][j] = cnt;
+					j++; 
+				}
+			}  // if 문 종료
+		} // 다이아 입력 for 문 종료
+		
+		for(int i = 0; i < n; i++) { //0과 다이아 2차원 배열 출력
+			for(int j = 0; j < n ; j ++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+}
